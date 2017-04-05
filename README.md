27f2227c0305dc97b106544f5688a2a1331c5d82
zhangruiqing01 <zhangruiqing01@baidu.com>
2017-04-05 17:56:06 +0800

    Initialize memnet

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # memnet
 Simplest end to end memory network implemented in tensorflow
+
+
+## File Structure
+
+### dataprovider.py
+#### Data Source:
+BABI, a simulated reading comprehension dataset with 20 tasks
+
+#### Data Download:
+wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz
+tar -zxf tasks_1-20_v1-2.tar.gz
+
+#### Data Structure
+We have two classes:
+1. GenData:
+	Load data for specific task(among the 20 tasks), build dictionary and train/test set, each set is an instance of Data class.
+
+2. Data:
+	Data class includes 3 attributes: h(history), q(query) and a(answer). This class implements a function to generate next batch each time.
+
+### memN2N.py
+The implementation of End-to-end-memory-network.
+
+### main.py
+Call memN2N.py for train.
+
---
+++ main.py
@@ -0,0 +1,36 @@
+import pprint
+
+import tensorflow as tf
+import numpy as np
+
+from dataprovider import *
+from memN2N import MemN2N
+
+flags = tf.app.flags
+flags.DEFINE_string("dataDir", "../memn2n/data/tasks_1-20_v1-2/en", "Directory of data bAbI")
+flags.DEFINE_integer("memory_size", 50, "memory size")
+flags.DEFINE_integer("description_length", 11, "max length of description/question")
+flags.DEFINE_integer("emb_size", 20, "embedding_size")
+flags.DEFINE_integer("vocab_size", -1, "specify size of vocabulary")
+flags.DEFINE_integer("batch_size", 32, "training batch size")
+flags.DEFINE_float("learning_rate", 0.01, "training learning rate")
+flags.DEFINE_integer("num_epoch", 10, "training epochs")
+
+Flags = flags.FLAGS
+pp = pprint.PrettyPrinter()
+pp.pprint(Flags.__flags)
+
+# define data
+data = GenData('../memn2n/data/tasks_1-20_v1-2/en', memory_size = Flags.memory_size, description_size = Flags.description_length, vocab_size = Flags.vocab_size, taskid = 8)
+Flags.vocab_size = len(data.vocab)
+
+# define model
+sess = tf.Session()
+memnet = MemN2N(Flags, sess)
+memnet.train(data)
+
+
+
+
+
+
